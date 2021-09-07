d316f789ce166c58ed8f313544835c506c06634b
Leonardo Alvarado Menendez <A01705998@itesm.mx>
2021-09-07 11:39:19 -0500

    Rama Login

---
+++ Proyecto/app/src/main/java/com/itesm/ic2007b/proyecto/Login.kt
@@ -0,0 +1,18 @@
+package com.itesm.ic2007b.proyecto
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.itesm.ic2007b.proyecto.databinding.ActivityLoginBinding
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
