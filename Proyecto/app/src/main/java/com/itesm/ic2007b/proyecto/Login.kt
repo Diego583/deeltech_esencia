48c4fc3fc73f4780f50d795a576f9ed35bd1088c
a01366802 <e1000lio2000@gmail.com>
2021-09-08 00:57:39 -0500

    comentarios de imports

--- Proyecto/app/src/main/java/com/itesm/ic2007b/proyecto/Login.kt
+++ Proyecto/app/src/main/java/com/itesm/ic2007b/proyecto/Login.kt
@@ -1,13 +1,13 @@
 package com.itesm.ic2007b.proyecto
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+//import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.itesm.ic2007b.proyecto.databinding.ActivityLoginBinding
+//import androidx.navigation.findNavController
+//import androidx.navigation.ui.AppBarConfiguration
+//import androidx.navigation.ui.navigateUp
+//import androidx.navigation.ui.setupActionBarWithNavController
+//import com.itesm.ic2007b.proyecto.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
 
