14a1bf581c6d56012292464ed907f53181720446
a01366802 <e1000lio2000@gmail.com>
2021-09-08 09:24:25 -0500

    agregarMamadas

--- Proyecto/app/src/main/java/com/itesm/ic2007b/proyecto/Login.kt
+++ Proyecto/app/src/main/java/com/itesm/ic2007b/proyecto/Login.kt
@@ -1,6 +1,10 @@
 package com.itesm.ic2007b.proyecto
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 //import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 //import androidx.navigation.findNavController
@@ -9,10 +13,14 @@ import androidx.appcompat.app.AppCompatActivity
 //import androidx.navigation.ui.setupActionBarWithNavController
 //import com.itesm.ic2007b.proyecto.databinding.ActivityLoginBinding
 
+var mail = arrayOf("e1000lio2000@gmail.com", "yaso@gmail.com")
+var pass = arrayOf("Aa123456789:v", "yas.02011")
+
 class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
 }
